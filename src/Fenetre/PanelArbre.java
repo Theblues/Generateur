abc66f4596ed8a3330dd95fd5744721a5754c36e
Florian Legros <kilakounet@gmail.com>
2012-01-10 18:07:32 +0100

    arbov1

--- src/Fenetre/PanelArbre.java
+++ src/Fenetre/PanelArbre.java
@@ -5,77 +5,55 @@ import java.io.*;
 import javax.swing.*;
 import javax.swing.tree.*;
 
-public class PanelArbre extends JPanel
+public class PanelArbre extends JPanel 
 {
-	private JTree					arbre;
-	private DefaultMutableTreeNode	racine;
+	private JTree arbre;
+	private DefaultMutableTreeNode racine;
 
-	public PanelArbre(JFrame f)
-	{
-		// On invoque la méthode de construction de notre arbre
+	public PanelArbre(JFrame f) {
 		listRoot(f);
 
 		add(arbre);
 	}
 
-	private void listRoot(JFrame f)
+	private void listRoot(JFrame f) 
 	{
 		this.racine = new DefaultMutableTreeNode();
+		File file = new File("./");
 
-		int count = 0;
-		for (File file : File.listRoots())
-		{
-			System.out.println(file.toString());
-			DefaultMutableTreeNode lecteur = new DefaultMutableTreeNode(file.getAbsolutePath());
-			try
-			{
-				for (File nom : file.listFiles())
-				{
-					DefaultMutableTreeNode node = new DefaultMutableTreeNode(nom.getName() + "\\");
-
-					lecteur.add(this.listFile(nom, node));
-
-				}
-			} catch (NullPointerException e)
-			{
+		DefaultMutableTreeNode lecteur = new DefaultMutableTreeNode(file.getAbsolutePath());
+		try {
+			for (File nom : file.listFiles()) {
+				DefaultMutableTreeNode node = new DefaultMutableTreeNode(nom.getName() + "\\");
+				lecteur.add(this.listFile(nom, node));
 			}
-
-			this.racine.add(lecteur);
-
-			// Si nous avons parcouru plus de 50 dossiers, on sort
-			 if(count > 50)
-				 break;
-
+		} catch (NullPointerException e) {
 		}
-		// On crée, avec notre hiérarchie, un arbre
+
+		this.racine.add(lecteur);
+
 		arbre = new JTree(this.racine);
-		
+
 		f.getContentPane().add(new JScrollPane(arbre));
 	}
 
-	private DefaultMutableTreeNode listFile(File file, DefaultMutableTreeNode node)
-	{
+	private DefaultMutableTreeNode listFile(File file,
+			DefaultMutableTreeNode node) {
 		int count = 0;
 
 		if (file.isFile())
 			return new DefaultMutableTreeNode(file.getName());
-		else
-		{
-			for (File nom : file.listFiles())
-			{
+		else {
+			for (File nom : file.listFiles()) {
 				count++;
 				// pas plus de 5 enfants par noeud
-				if (count < 5)
-				{
+				if (count < 5) {
 					DefaultMutableTreeNode subNode;
-					if (nom.isDirectory())
-					{
+					if (nom.isDirectory()) {
 						subNode = new DefaultMutableTreeNode(nom.getName()
 								+ "\\");
 						node.add(this.listFile(nom, subNode));
-					}
-					else
-					{
+					} else {
 						subNode = new DefaultMutableTreeNode(nom.getName());
 					}
 					node.add(subNode);
