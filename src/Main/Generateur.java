634382050225d18203e020506145c19f1a87ea7b
Theblues <erwan76.lebrun@gmail.com>
2012-01-10 23:11:33 +0100

    Realisation de gros

--- src/Main/Generateur.java
+++ src/Main/Generateur.java
@@ -1,25 +1,51 @@
 package Main;
 
-import Fenetre.*;
+import Utilitaire.*;
+import IHM.Frame.*;
 
 public class Generateur
 {
 	private static Fenetre fenetre;
+	private static Generator generator;
 
 	public Generateur()
 	{
 		fenetre = new Fenetre();
+		generator = new Generator();
 	}
 	
 	public static Fenetre getFenetre()	{		return fenetre;		}
 	
-	public static void FenetreCreerPage()
+	public static void FenetreCreerPage()			{		new FenetreCreerPage();			}
+	public static void FenetreAjouterParagraphe()	{		new FenetreAjouterParagraphe();	}
+	public static void FenetreAjouterTitre()		{		new FenetreAjouterTitre();		}
+	
+	public static void ajouterParagraphe(String text)
 	{
-		new FenetreCreerPage();
+		// TODO Auto-generated method stub
+		generator.addParagraphe(text);
+	}
+	
+	public static void ajouterTitre(String text)
+	{
+		// TODO Auto-generated method stub
+		generator.addTitre(text);
+	}
+	
+	public static void generer()
+	{
+		// TODO Auto-generated method stub
+		generator.generate();
 	}
 	
 	public static void main(String[] argv)
 	{
 		new Generateur();
 	}
+
+	
+
+	
+
+	
 }
