38dbbc474d6dce96847a5bf80a12ae1b9159842f
Theblues <erwan76.lebrun@gmail.com>
2012-01-10 17:23:12 +0100

    init

---
+++ src/Fenetre/Fenetre.java
@@ -0,0 +1,48 @@
+package Fenetre;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Fenetre extends JFrame
+{
+	private PanelMenu 			menu;
+	private PanelArbre			arborescence;
+	private JSplitPane 			splitPaneVertical;
+	private JSplitPane 			splitPaneTotal;
+	
+	public Fenetre()
+	{
+		setTitle("Generateur de site");
+		setSize(1024, 700);
+		//setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		menu = new PanelMenu();
+		menu.addMenuInFrame(this);
+		
+		arborescence = new PanelArbre();
+		
+		splitPaneTotal = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, arborescence, new Panel());
+		splitPaneTotal.setOneTouchExpandable(true);
+		
+		add(splitPaneTotal);
+		// permet l'action de la croix rouge
+		addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e) 
+            {
+                System.exit(0);
+            }
+        });
+
+		setVisible(true);
+	}
+
+	public PanelMenu getMenu()					{		return menu;			}
+	public PanelArbre getArborescence()			{		return arborescence;	}
+	
+	public static void main(String[] argv)
+	{
+		new Fenetre();
+	}
+}
