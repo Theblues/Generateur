4c2bb543e622087f67cd99eea12f40df551da52e
Florian Legros <kilakounet@gmail.com>
2012-01-11 11:12:12 +0100

    fix a zour1

--- src/IHM/Panel/PanelArbre.java
+++ src/IHM/Panel/PanelArbre.java
@@ -65,7 +65,7 @@ public class PanelArbre extends JPanel
 	public void ajoutFils(String s, String value) {
 		DefaultTreeModel dtm = new DefaultTreeModel(racine);
 		Object parent = dtm.getChild(racine,0);
-		Object parent2 = dtm.getChild(parent, 1);
+		Object parent2 = dtm.getChild(parent,0);
 
 		DefaultMutableTreeNode mtn = new DefaultMutableTreeNode(new File(s));
 		dtm.insertNodeInto(mtn,(MutableTreeNode) parent2,cpt);
